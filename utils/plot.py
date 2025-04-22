8a4f375f2d1f3d068166dd62dffbc45c412e5b5c
Xinyuan Li <1501454486@qq.com>
2025-04-22 15:13:09 +0800

    feat: add plot_values function in plot.py, which helps plot figures given values, such as losses figures and accuracy figures

--- utils/plot.py
+++ utils/plot.py
@@ -30,3 +30,21 @@ def plot_temperature(temperatures, next_token_logits, vocab):
     ax.legend()
     plt.tight_layout()
     plt.show()
+
+
+def plot_values(epoch_seen, examples_seen, train_values, val_values, label = "loss"):
+    fig, ax1 = plt.subplots(figsize = (5, 3))
+    ax1.plot(epoch_seen, train_values, label = f"Training {label}")
+    ax1.plot(epoch_seen, val_values, linestyle = "-.", label = f"Validation {label}")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    ax2 = ax1.twiny()
+    ax2.plot(examples_seen, train_values, alpha = 0)
+
+    ax2.set_xlabel("Examples seen")
+
+    fig.tight_layout()
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
