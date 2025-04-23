4790ea8a259fc238791390dd36491780fe99a933
Xinyuan Li <1501454486@qq.com>
2025-04-23 18:16:18 +0800

    feat: - add custom_collate_fn in dataset.py, which adds ignore_index and   allowed_max_length compared to previous version

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -144,3 +144,41 @@ def custom_collate_draft_2(batch, pad_token_id = 50256, device = "cpu"):
     inputs_tensor = torch.stack(inputs_list).to(device)
     targets_tensor = torch.stack(targets_list).to(device)
     return inputs_tensor, targets_tensor
+
+
+def custom_collate_fn(
+    batch, 
+    pad_token_id = 50256, 
+    ignore_index = -100, 
+    allowed_max_length = None, 
+    device = "cpu"
+):
+    batch_max_length = max(len(item) + 1 for item in batch)
+    inputs_list, targets_list = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        # pad sequence to max length
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        # replace all but the first padding tokens to ignore_index
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        # optionally truncate to maximum sequence length
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_list.append(inputs)
+        targets_list.append(targets)
+
+    inputs_tensor = torch.stack(inputs_list).to(device)
+    targets_tensor = torch.stack(targets_list).to(device)
+
+    return inputs_tensor, targets_tensor
